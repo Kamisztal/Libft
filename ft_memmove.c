957fb16d05fa349d0aa57913dc377776839325bb
Patrick <patrick.misztalf2@gmail.com>
2026-08-04 03:15:02 +0200

    memory learning

---
+++ ft_memmove.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmisztal <patrick.misztal@learner.42.te    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/04 02:48:38 by pmisztal          #+#    #+#             */
+/*   Updated: 2026/08/04 02:48:38 by pmisztal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *ft_memmove(void* dest, const void* src, size_t count )
+{
+    size_t i;
+    unsigned char *dst;
+    const unsigned char *sr;
+
+    dst = (unsigned char *)dest;
+    sr = (unsigned char *)src;
+    i = 0;
+    while (i < count)
+    {
+        dst[i] = sr[i];
+        i++;
+    }
+    return (dest);
+}
+
+/*
+#include <stdio.h>
+int main (void)
+{
+    char src[7] = "Hellooo";
+    char dest[8] = "nothing";
+
+    ft_memmove(dest, src, 9);
+    printf("%s\n", dest);
+    return (0);
+}
+*/
---
+++ ft_memset.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmisztal <patrick.misztal@learner.42.te    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/03 01:37:20 by pmisztal          #+#    #+#             */
+/*   Updated: 2026/08/03 01:37:20 by pmisztal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *ft_memset(void *s, int c, size_t n)
+{
+    size_t i;
+    unsigned char *ptr;
+   
+    ptr = (unsigned char *)s;
+    i = 0;
+
+    while (i < n)
+    {
+        ptr[i] = (unsigned char)c;
+        i++;
+    }
+    return (ptr);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+    char buffer[11] = "42liblfwfef";
+ 
+    ft_memset(buffer, 'A', 11);
+    printf("%s\n", buffer);
+    return (0);
+}
+*/
