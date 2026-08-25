1136f39d94fc672c3242733618c63faf896eb3e5
Patrick <patrick.misztalf2@gmail.com>
2026-08-25 22:54:33 +0200

    .h

--- libft.h
+++ libft.h
@@ -30,6 +30,7 @@ int     ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
+int     ft_atoi(const char *str);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
