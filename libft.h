970b633ac2c1eaf9cf237ae782803e6775b8088f
Patrick <patrick.misztalf2@gmail.com>
2026-09-02 23:25:36 +0200

    .h

--- libft.h
+++ libft.h
@@ -43,4 +43,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
 #endif
