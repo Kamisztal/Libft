16218bd1aab5c35117bb37c4f8e244a581bd4c9e
Patrick <patrick.misztalf2@gmail.com>
2026-09-08 22:53:24 +0200

    fd end

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmisztal <patrick.misztal@learner.42.te    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/08 22:30:39 by pmisztal          #+#    #+#             */
+/*   Updated: 2026/09/08 22:30:39 by pmisztal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = n * -1;
+	}
+	if (n <= 9 && n >= 0)
+		ft_putchar_fd(n + '0', fd);
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+}
+/*
+int	main(void)
+{
+	int	nb;
+
+	nb = 42;
+	ft_putnbr_fd(nb, 1);
+	return (0);
+}
+*/
