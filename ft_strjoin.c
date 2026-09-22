4e5415520249a9442d6b9e6d072cbe2fd4b00b09
Patrick <patrick.misztalf2@gmail.com>
2026-09-22 10:30:12 +0200

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,11 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strjoin(char const *s1, char const *s2)
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		debut;
 	int		i;
 	int		j;
 	char	*str;
 
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char	*str;
+	char	*str2;
+
+	str = "Hello";
+	str2 = "World";
+	printf("%s", ft_strjoin(str, str2));
+	return (0);
+}
+*/
