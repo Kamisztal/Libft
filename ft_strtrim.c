2733e07321048e6a19c8497be79205ff082feed0
Patrick <patrick.misztalf2@gmail.com>
2026-09-24 17:00:45 +0200

    trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,5 +14,42 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int		i;
+	int		debut;
+	int		len;
+	char	*str;
 
+	len = ft_strlen(s1);
+	i = 0;
+	str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0' && ft_strchr(set, s1[i]) != NULL)
+		i++;
+	debut = i;
+	while (len - 1 > debut && ft_strchr(set, s1[len]) != NULL)
+		len--;
+	i = 0;
+	while (debut <= len)
+	{
+		str[i] = s1[debut];
+		i++;
+		debut++;
+	}
+	str[i] = '\0';
+	return (str);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char *set;
+	char *str;
+
+	set = "x";
+	str = "xxHelxloxx";
+	printf("%s", ft_strtrim(str, set));
+	return (0);
+}
+*/
