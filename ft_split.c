21839188c72f93138d92bf57d831bf7165841a74
Patrick <patrick.misztalf2@gmail.com>
2026-09-25 21:13:25 +0200

    split d

--- ft_split.c
+++ ft_split.c
@@ -10,9 +10,78 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-char **ft_split(char const *s, char c)
+int	is_sep(char s, char c)
 {
+	if (s == c)
+		return (0);
+	else
+		return (1);
+}
 
+int	wordcount(char *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != '\0' && is_sep(s[i], c) == 1)
+		{
+			count++;
+			while (s[i] != '\0' && is_sep(s[i], c) == 1)
+				i++;
+		}
+		while (s[i] != '\0' && is_sep(s[i], c) == 0)
+			i++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		debut;
+	char	**str;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	str = malloc(sizeof(char *) * (wordcount(s, c) + 1));
+	if (!str)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] != '\0' && is_sep(s[i], c) == 1)
+		{
+			debut = i;
+			while (s[i] != '\0' && is_sep(s[i], c) == 1)
+				i++;
+			str[j] = ft_substr(s, debut, i - debut);
+			if (str[j] == NULL)
+			{
+				while (k < j)
+				{
+					free(str[k]);
+					k++;
+				}
+				free(str);
+				return (NULL);
+			}
+			j++;
+		}
+		while (s[i] != '\0' && is_sep(s[i], c) == 0)
+			i++;
+	}
+	str[j] = NULL;
+	return (str);
+}
+
+int	main(void)
+{
 }
